8a03afd6c7ff5e5418a6c43a34d2a87079e12852
Marvin Schramm <marvin@kreait.com>
2021-02-15 21:40:42 +0100

    move tests into the correct package

--- spring-json-api-starter/build.gradle.kts
+++ spring-json-api-starter/build.gradle.kts
@@ -4,5 +4,4 @@ dependencies {
 
     testImplementation(group = "org.springframework.boot", name = "spring-boot-starter-test")
     testImplementation(group = "org.springframework.boot", name = "spring-boot-starter-web")
-    testImplementation("io.mockk:mockk:1.10.6")
 }
--- spring-json-api/build.gradle.kts
+++ spring-json-api/build.gradle.kts
@@ -1,3 +1,5 @@
 dependencies {
     optional(group = "org.springframework.boot", name = "spring-boot-starter-web")
+
+    testImplementation("io.mockk:mockk:1.10.6")
 }
